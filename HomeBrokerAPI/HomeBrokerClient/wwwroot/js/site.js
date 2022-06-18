70d602fdabdf8f2ffea62b8a8a7599ddf03d1c68
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2022-06-18 16:29:53 -0300

    form input validation

--- HomeBrokerAPI/HomeBrokerClient/wwwroot/js/site.js
+++ HomeBrokerAPI/HomeBrokerClient/wwwroot/js/site.js
@@ -1,9 +1,7 @@
-﻿function ofertasTicker() {
-
-    return false;
-}
-
-
+﻿/*
+  This function calculates and updates the Total information when
+  amount or price is changed.
+ */   
 function atualizaTotal() {
     var pQtd = $('#inAmount').val();
     var pPreco = $('#inPrice').val();
@@ -15,10 +13,63 @@ function atualizaTotal() {
     $('#inTotal').val(parseFloat(pTotal).toFixed(2));
 }
 
-function validaDados() {
+/*
+  This function cleans Error Messages.
+ */
+function limpaMsgErro() {
+    $('#errorMessage').hide();
+}
+
+/*
+   This function checks for valid parameters before submitting form data 
+ */
+function validaDados(pTipo) {
+    $('#errorMessage').hide();
+    
     var pData = $('#inDate').val();
-    var pTicker = $('#inTickerOrder').val();
+    var pTicker = $('#inTickerOrder').val().trim();
     var pQtd = $('#inAmount').val();
     var pPreco = $('#inPrice').val();
-    var pAssinatura = $('#inSign').val();
+    var pAssinatura = $('#inSign').val().trim();
+
+    if ($.isNumeric(pQtd)) {
+        pQtd = parseInt(pQtd);
+        $('#inAmount').val(pQtd);
+    }
+
+    if ($.isNumeric(pPreco)) {
+        pPreco = parseFloat(pPreco).toFixed(2);
+        $('#inPrice').val(pPreco);
+    }        
+    
+    if (pTicker === "") {
+        $('#errorMessage').html("Informe o código da ação (Ticker)!");
+        $('#errorMessage').show();
+        return false;
+    }
+
+    if (!$.isNumeric(pQtd) || (pQtd <= 0)) {
+        $('#errorMessage').html("A quantidade deve ser um valor numérico positivo!");
+        $('#errorMessage').show();
+        return false;
+    }
+
+    if ((pQtd % 100) != 0) {
+        $('#errorMessage').html("Mercado fracionário não permitido. Informe quantidades de lote padrão!");
+        $('#errorMessage').show();
+        return false;
+    }
+
+    if (!$.isNumeric(pPreco) || (pPreco <= 0)) {
+        $('#errorMessage').html("O preço deve ser um valor numérico positivo!");
+        $('#errorMessage').show();
+        return false;
+    }
+
+    if (pAssinatura === "") {
+        $('#errorMessage').html("Informe a assinatura eletrônica!");
+        $('#errorMessage').show();
+        return false;
+    }
+    
 }
