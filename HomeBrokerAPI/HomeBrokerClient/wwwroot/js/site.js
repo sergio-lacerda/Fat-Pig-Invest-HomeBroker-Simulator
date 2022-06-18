28874399a387296a9eda9e9d57efa824895d75e3
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2022-06-18 12:38:45 -0300

    fixing ticker search on offers book

--- HomeBrokerAPI/HomeBrokerClient/wwwroot/js/site.js
+++ HomeBrokerAPI/HomeBrokerClient/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function ofertasTicker() {
 
-// Write your JavaScript code.
+    return false;
+}
+
+
+function atualizaTotal() {
+    var pQtd = $('#inAmount').val();
+    var pPreco = $('#inPrice').val();
+    var pTotal = 0;
+
+    if ($.isNumeric(pQtd) && $.isNumeric(pPreco) && (pQtd > 0) && (pPreco > 0)) {
+        pTotal = pQtd * pPreco;        
+    }
+    $('#inTotal').val(parseFloat(pTotal).toFixed(2));
+}
+
+function validaDados() {
+    var pData = $('#inDate').val();
+    var pTicker = $('#inTickerOrder').val();
+    var pQtd = $('#inAmount').val();
+    var pPreco = $('#inPrice').val();
+    var pAssinatura = $('#inSign').val();
+}
